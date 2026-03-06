57f7f7425dc925c9e6c4ff7c54057e865e67f42b
Jesse newton <chronosllc0@gmail.com>
2026-03-06 04:05:11 +0100

    Keep Smartsupp launcher floating and visible from mobile hero

--- src/supplementsApp.js
+++ src/supplementsApp.js
@@ -11,7 +11,7 @@ import {
   updateQuantity,
 } from './components/cartStore.js'
 import { icon } from './components/icons.js'
-import { escapeHtml, focusTrap, scrollLock, submitConsultationRequest } from './components/utils.js'
+import { escapeHtml, focusTrap, scrollLock, setupRevealTransitions, submitConsultationRequest } from './components/utils.js'
 import { SITE_CONTENT } from './data/content.js'
 
 const FOOTER_SECTION_MAP = {
@@ -402,6 +402,8 @@ function productModalTemplate(product) {
 
 export function mountSupplementsApp(root, content) {
   root.innerHTML = renderSupplementsLayout(content)
+
+  setupRevealTransitions(root)
 
   const products = new Map(content.supplements.map((item) => [item.id, item]))
   const productsPerPage = 9
